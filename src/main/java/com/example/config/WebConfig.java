1e47da5462f18952e33e40f76e8fbc527738ab24
LAGUNHO <100767288+gunho30811@users.noreply.github.com>
2025-01-15 16:07:25 +0900

    gitignore 추가 및 jetty 버젼 다운

--- src/main/java/com/example/config/WebConfig.java
+++ src/main/java/com/example/config/WebConfig.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import jakarta.servlet.Filter;
-
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
